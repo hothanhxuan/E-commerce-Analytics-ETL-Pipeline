aa43fd7cb1c0301a7b819ad4c2132455efd0645e
Ho Thanh Xuan <htxuan842@gmail.com>
2026-05-09 19:47:06 +0700

    update

--- loaders/bigquery_loader.py
+++ loaders/bigquery_loader.py
@@ -215,9 +215,9 @@ class BigQueryLoader:
             GROUP BY customer_id
         )
         SELECT
-            e.customer_id,
-            c.full_name,
-            c.customer_segment,
+            e.customer_id,\r
+            COALESCE(c.full_name, 'Unknown') AS full_name,\r
+            COALESCE(c.customer_segment, 'Unknown') AS customer_segment,
             e.session_id,
             e.event_type,
             e.event_timestamp,
